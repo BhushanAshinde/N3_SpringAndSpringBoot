eb00fd796c053ced0005af78eda34e6926b5f66c
BhushanAshinde <92881526+BhushanAshinde@users.noreply.github.com>
2023-04-12 11:30:57 +0530

    fifth commit

---
+++ SpringWebAppUsingMVCc/src/main/WebApp/home.jsp
@@ -0,0 +1,8 @@
+<html>
+<head> <title> MVC </title></head>
+<body>
+
+<h1> 1st heading </h1> 
+</body
+
+</html>
---
+++ SpringWebAppUsingMVCc/src/main/java/org/tnsif/springwebmvc/SpringWebAppUsingMvCcApplication.java
@@ -0,0 +1,13 @@
+package org.tnsif.springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppUsingMvCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppUsingMvCcApplication.class, args);
+	}
+
+}
